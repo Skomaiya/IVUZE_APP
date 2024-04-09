31ad3db7d78023ff454ba41120d812a10b51fda9
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-04-09 07:55:30 +0200

    ivuze

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+import mongoose from "mongoose"
+
+const app = express()
+
+const port =4000
+const db = "mongodb+srv://Mariya:KKXp3IWxu7Sbp5zi@cluster0.fslpg5p.mongodb.net/IVUZE_App"
+
+mongoose.connect(db)
+    try{
+        console.log("DB connected successfully")
+        app.listen(port,()=>{
+            console.log(`server is running on port ${port} .... `)
+        })
+
+    }
+    catch(err){
+        console.log(err)
+    }
+
