d8ef0c25e621429397b5cdd8a2ba8473ee8e5476
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-04-09 12:38:46 +0200

    controller

--- controllers/ivuze.controller.js
+++ controllers/ivuze.controller.js
@@ -1,102 +1,96 @@
-const ivuzeModel = require('../models/ivuze.model');
-
-const create = async (req, res, next) => {
-    try {
-        const { name, email, role, hospitalCategory, password } = req.body;
-
-        var alreadyExists = await ivuzeModel.findOne({ 'email': email });
-        console.log(email);
-        if (!alreadyExists) {
-            const addedUser = await ivuzeModel.create(req.body);
-
-            console.log(addedUser);
-            res.status(201).json({
-                message: "User added successfully",
-                addedUser
+import ivuzeModel from "../model/ivuze.model";
+const ivuzeController = {
+     create : async (req, res, next) => {
+        try {
+          
+    
+            var alreadyExists = await ivuzeModel.findOne({ 'email': email });
+            console.log(email);
+            if (!alreadyExists) {
+                const addedUser = await ivuzeModel.create(req.body);
+    
+                console.log(addedUser);
+                res.status(201).json({
+                    message: "User added successfully",
+                    addedUser
+                });
+            } else {
+                res.status(401).send({ message: "This user already exists", error: console.error.message });
+            }
+        } catch (error) {
+            res.status(500).send(error.message);
+        }
+    },
+    
+     update : async (req, res, next) => {
+        try {
+            var updatedUser = await ivuzeModel.findOneAndUpdate({ _id: req.query.id }, req.body);
+            var user = await ivuzeModel.findOne(updatedUser._id);
+            res.status(200).json({ user });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+    
+     list : async (req, res, next) => {
+        try {
+            var users = await ivuzeModel.find({});
+            res.status(200).json({ users });
+        } catch (error) {
+            res.status(500).send("Error in listing users");
+        }
+    },
+    
+    findByFullName : async (req, res, next) => {
+        try {
+            console.log(req.query);
+            let userName = req.query.name;
+            var foundUser = await ivuzeModel.findOne({ name: userName });
+            res.status(302).json({
+                data: foundUser,
             });
-        } else {
-            res.status(401).send({ message: "This user already exists", error: console.error.message });
+        } catch (error) {
+            res.status(500).send(error.message);
         }
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
-
-const update = async (req, res, next) => {
-    try {
-        var updatedUser = await ivuzeModel.findOneAndUpdate({ _id: req.query.id }, req.body);
-        var user = await ivuzeModel.findOne(updatedUser._id);
-        res.status(200).json({ user });
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
-
-const list = async (req, res, next) => {
-    try {
-        var users = await ivuzeModel.find({});
-        res.status(200).json({ users });
-    } catch (error) {
-        res.status(500).send("Error in listing users");
-    }
-}
-
-const findByFullName = async (req, res, next) => {
-    try {
-        console.log(req.query);
-        let userName = req.query.name;
-        var foundUser = await ivuzeModel.findOne({ name: userName });
-        res.status(302).json({
-            data: foundUser,
-        });
-    } catch (error) {
-        res.status(500).send(error.message);
-    }
-}
-
-const findById = async (req, res, next) => {
-    try {
-        let userId = req.query.id;
-        var foundUser = await ivuzeModel.findById({ _id: userId });
-        res.status(200).json({
-            user: foundUser
-        });
-    } catch (error) {
-        res.status(500).send(error);
-    }
-};
-
-const findByNationalId = async (req, res, next) => {
-    try {
-        let userId = req.body.nationalId;
-        var foundUser = await ivuzeModel.findOne({ nationalId: userId });
-        res.status(200).json({
-            user: foundUser
-        });
-    } catch (error) {
-        res.status(500).send(error);
-    }
-}
-
-const remove = async (req, res, next) => {
-    try {
-        var deletedUser = await ivuzeModel.findByIdAndDelete(req.query.id);
-        if (deletedUser) {
-            res.status(200).json({ message: "Deleted!" });
-        } else {
-            res.status(400).json({ message: "User not found!" });
+    },
+    
+    findById : async (req, res, next) => {
+        try {
+            let userId = req.query.id;
+            var foundUser = await ivuzeModel.findById({ _id: userId });
+            res.status(200).json({
+                user: foundUser
+            });
+        } catch (error) {
+            res.status(500).send(error);
         }
-    } catch (error) {
-        res.status(500).send(error);
+    },
+    
+     findByNationalId : async (req, res, next) => {
+        try {
+            let userId = req.body.nationalId;
+            var foundUser = await ivuzeModel.findOne({ nationalId: userId });
+            res.status(200).json({
+                user: foundUser
+            });
+        } catch (error) {
+            res.status(500).send(error);
+        }
+    },
+    
+    remove : async (req, res, next) => {
+        try {
+            var deletedUser = await ivuzeModel.findByIdAndDelete(req.query.id);
+            if (deletedUser) {
+                res.status(200).json({ message: "Deleted!" });
+            } else {
+                res.status(400).json({ message: "User not found!" });
+            }
+        } catch (error) {
+            res.status(500).send(error);
+        }
     }
+    
 }
 
-module.exports = {
-    create,
-    update,
-    list,
-    findByFullName,
-    findById,
-    findByNationalId,
-    remove
-};
+export default ivuzeController
