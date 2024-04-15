31a36e46da71e232eb7452ee5ef846836a7a1e69
MAHAMAT263 <m.ahmat@alustudent.com>
2024-04-15 15:45:25 +0300

    Booking

--- components/hospitals.js
+++ components/hospitals.js
@@ -1,3 +1,18 @@
+const hamburger = document.querySelector('.open');
+const close = document.querySelector('.close');
+
+close.onclick = () => {
+    document.querySelector('.nav-menu ul').classList.remove('active');
+    close.style.display = 'none';
+    hamburger.style.display = 'block';
+}
+
+hamburger.onclick = () => {
+    document.querySelector('.nav-menu ul').classList.toggle('active');
+    close.style.display = 'block';
+    hamburger.style.display = 'none';
+};
+
 function setupLikeFunction(a, b) {
     var likes= document.getElementById(a);
     
@@ -39,3 +54,4 @@ views.addEventListener("click", function() {
 
 })};
 setupviewFunction("view","numview")
+
--- components/login.js
+++ components/login.js
@@ -28,7 +28,22 @@ form.addEventListener('submit', e => {
         form.reset()
     })
     .catch(error => console.error('Error!', error.message))
-})
+});
+
+const hamburger = document.querySelector('.open');
+const close = document.querySelector('.close');
+
+close.onclick = () => {
+    document.querySelector('.nav-menu ul').classList.remove('active');
+    close.style.display = 'none';
+    hamburger.style.display = 'block';
+}
+
+hamburger.onclick = () => {
+    document.querySelector('.nav-menu ul').classList.toggle('active');
+    close.style.display = 'block';
+    hamburger.style.display = 'none';
+};
 
 
 
